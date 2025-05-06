ca472457f10dcf708b98996865e58905cb2ea9f7
pyounani <yndbsk9372@gmail.com>
2025-05-06 20:22:12 +0900

    feat: make router about music

--- backend/app/api/v1/music_api.py
+++ backend/app/api/v1/music_api.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from app.services import music_service
+from app.services.music_service import MusicService
+from app.core.database import get_mongo_client
 
 router = APIRouter()
 
@@ -9,8 +10,11 @@ class PromptRequest(BaseModel):
     prompt: str
 
 
-@router.post("/")
-async def generate_music(request: PromptRequest):
-    return music_service.generate_music(request.prompt)
-
-
+@router.post("/users/{user_key}/music")
+async def generate_music(
+        user_key: str,
+        request: PromptRequest,
+        db=Depends(get_mongo_client)
+):
+    music_service = MusicService(db)
+    return await music_service.generate_music(prompt=request.prompt, user_key=user_key)
