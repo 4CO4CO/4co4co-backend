2ba552b512e2f5c6a140cc1ac8dda84319570bb4
pyounani <yndbsk9372@gmail.com>
2025-05-10 00:01:25 +0900

    feat: change to s3 in creating lantern api (#22)

--- backend/app/services/lantern_service.py
+++ backend/app/services/lantern_service.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from uuid import uuid4
 
 from app.core.exceptions.types import FileSaveError, ValidationError, NotFoundError
+from app.core.s3 import upload_file_to_s3
 from app.core.tasks.panorama_tasks import generate_panorama_task
 from app.repositories.lantern_repository import LanternRepository
 from app.repositories.music_repository import MusicRepository
@@ -10,9 +11,6 @@ from app.repositories.panorama_repository import PanoramaRepository
 from app.schemas.db.lantern import LanternDBModel
 from app.schemas.response.lantern_detail_response import LanternDetailResponseModel
 from app.schemas.response.lantern_list_response import LanternListResponseModel
-
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 class LanternService:
@@ -25,13 +23,13 @@ class LanternService:
         if not name.strip():
             raise ValidationError("Name is required")
 
-        file_path, original_filename, file_extension, file_size = await self._save_file(image)
+        file_url, original_filename, file_extension, file_size = await self._upload_image_to_s3(image)
         lantern_id = str(uuid4())
 
         user_model = LanternDBModel(
             lantern_id=lantern_id,
             user_name=name,
-            image_path=file_path,
+            image_path=file_url,
             original_filename=original_filename,
             file_extension=file_extension,
             file_size=file_size,
@@ -41,7 +39,7 @@ class LanternService:
         await self.lantern_repo.insert_lantern(user_model.model_dump(exclude={'id'}))
 
         # Celery 비동기 작업 실행 추가
-        generate_panorama_task.delay(prompt=name, lantern_id=lantern_id, image_path=file_path)
+        # generate_panorama_task.delay(prompt=name, lantern_id=lantern_id, image_path=file_path)
 
         return lantern_id
 
@@ -89,20 +87,18 @@ class LanternService:
         )
 
     @staticmethod
-    async def _save_file(image):
+    async def _upload_image_to_s3(image):
         original_filename = image.filename
-        file_extension = os.path.splitext(original_filename)[1]
-        saved_filename = f"{uuid4()}{file_extension}"
-        file_path = os.path.join(UPLOAD_DIR, saved_filename)
+        file_extension = original_filename.split('.')[-1]
 
         try:
-            content = await image.read()
-            file_size = len(content)
-            await image.seek(0)
+            file_url, file_size = await upload_file_to_s3(image, folder="lanterns")
+            if file_url is None:
+                raise FileSaveError("S3 upload failed")
 
-            with open(file_path, "wb") as buffer:
-                buffer.write(content)
         except Exception as e:
-            raise FileSaveError(f"File saving failed: {e}") from e
+            raise FileSaveError(f"File upload failed: {e}") from e
 
-        return file_path, original_filename, file_extension, file_size
+        return file_url, original_filename, file_extension, file_size
+
+
