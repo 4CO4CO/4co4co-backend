98f43db1f660ba9b13c0a9bc883a60008adc7b65
pyounani <yndbsk9372@gmail.com>
2025-06-28 16:07:11 +0900

    remove: remove v1 music api #44

--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1 import music_api, lantern_api
+from app.api.v1 import lantern_api
 
 api_router = APIRouter()
 
-api_router.include_router(music_api.router, tags=["music"])
 api_router.include_router(lantern_api.router, tags=["lanterns"])
 
